eb25fd88c2583487229dca27c9caae5dd51ab445
Rodrigo <rodrigombsoares@gmail.com>
2025-03-25 19:49:10 -0300

    feature(deployment): Add Aptos to memory test environment

--- deployment/environment/memory/node.go
+++ deployment/environment/memory/node.go
@@ -221,6 +221,7 @@ func NewNode(
 	port int, // Port for the P2P V2 listener.
 	chains map[uint64]deployment.Chain,
 	solchains map[uint64]deployment.SolChain,
+	aptoschains map[uint64]deployment.AptosChain,
 	logLevel zapcore.Level,
 	bootstrap bool,
 	registryConfig deployment.CapabilityRegistryConfig,
@@ -290,6 +291,16 @@ func NewNode(
 		}
 		c.Solana = solConfigs
 
+		var aptosConfigs chainlink.RawConfigs
+		for chainID, chain := range aptoschains {
+			aptosChainID, err := chainsel.GetChainIDFromSelector(chainID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			aptosConfigs = append(aptosConfigs, createAptosChainConfig(aptosChainID, chain))
+		}
+		c.Aptos = aptosConfigs
+
 		for _, opt := range configOpts {
 			opt(c)
 		}
@@ -339,7 +350,7 @@ func NewNode(
 		AuditLogger:              audit.NoopLogger,
 	})
 	require.NoError(t, err)
-	keys := CreateKeys(t, app, chains, solchains)
+	keys := CreateKeys(t, app, chains, solchains, aptoschains)
 
 	// JD
 
@@ -349,6 +360,7 @@ func NewNode(
 		Chains: slices.Concat(
 			maps.Keys(chains),
 			maps.Keys(solchains),
+			maps.Keys(aptoschains),
 		),
 		Keys:       keys,
 		Addr:       net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: port},
@@ -368,6 +380,7 @@ func CreateKeys(t *testing.T,
 	app chainlink.Application,
 	chains map[uint64]deployment.Chain,
 	solchains map[uint64]deployment.SolChain,
+	aptoschains map[uint64]deployment.AptosChain,
 ) Keys {
 	ctx := t.Context()
 	_, err := app.GetKeyStore().P2P().Create(ctx)
@@ -492,6 +505,28 @@ func CreateKeys(t *testing.T,
 		transmitters[chain] = transmitter.ID()
 
 		// TODO: funding
+	}
+
+	if len(aptoschains) > 0 {
+		ctype := chaintype.Aptos
+		err = app.GetKeyStore().OCR2().EnsureKeys(ctx, ctype)
+		require.NoError(t, err)
+		keys, err := app.GetKeyStore().OCR2().GetAllOfType(ctype)
+		require.NoError(t, err)
+		require.Len(t, keys, 1)
+		keybundle := keys[0]
+		keybundles[ctype] = keybundle
+
+		err = app.GetKeyStore().Aptos().EnsureKey(ctx)
+		require.NoError(t, err, "failed to create key for Aptos")
+
+		aptoskeys, err := app.GetKeyStore().Aptos().GetAll()
+		require.NoError(t, err)
+		require.Len(t, aptoskeys, 1)
+		transmitter := aptoskeys[0]
+		for chainSelector := range aptoschains {
+			transmitters[chainSelector] = transmitter.ID()
+		}
 	}
 
 	return Keys{
