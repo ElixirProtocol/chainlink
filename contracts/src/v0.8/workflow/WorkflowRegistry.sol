87fe77d7c99cdf36620ddd13f7821ca801e003d5
pavel-raykov <pavel.raykov@smartcontract.com>
2025-03-27 08:08:06 +0100

    Test.

--- contracts/src/v0.8/workflow/WorkflowRegistry.sol
+++ contracts/src/v0.8/workflow/WorkflowRegistry.sol
@@ -8,6 +8,7 @@ import {Ownable2StepMsgSender} from "../shared/access/Ownable2StepMsgSender.sol"
 import {Strings} from "../vendor/openzeppelin-solidity/v5.0.2/contracts/utils/Strings.sol";
 import {EnumerableSet} from "../vendor/openzeppelin-solidity/v5.0.2/contracts/utils/structs/EnumerableSet.sol";
 
+// My test
 contract WorkflowRegistry is Ownable2StepMsgSender, ITypeAndVersion {
   using EnumerableSet for EnumerableSet.Bytes32Set;
   using EnumerableSet for EnumerableSet.AddressSet;
